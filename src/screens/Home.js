5216df3fdc47665b99607b60cac31511bd1f5155
Fauzan <fauzan_lubis23@yahoo.com>
2019-10-27 13:05:28 +0800

    update menu

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -47,12 +47,14 @@ export default class DrawerExample extends Component {
                 </Header>
                
                     <Content>
-                    <View style={{alignItems:'center'}}>
-                    <ImageBackground source={require('../assets/bg2.png')} style={{
-        			 justifyContent:'center', alignItems:'center',
-			        width:420,
-			        height: 700}}  resizeMode="stretch">
-		      			<View>
+       
+        <View style={{alignItems:'center'}}>
+
+        <ImageBackground source={require('../assets/bg2.png')} style={{
+		 justifyContent:'center', alignItems:'center',
+        width:420,
+        height: 550}}  resizeMode="stretch">
+		      			
 	                   
 			           
 	                	<View style={{ alignItems:'center',flexDirection:'column', flex:1, marginTop:20}}>
@@ -85,7 +87,7 @@ export default class DrawerExample extends Component {
 
 
 
-	                	<View style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+	                <View style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
 
 							<View>
 							<TouchableOpacity style={{margin:2, borderRadius:13, width:80, height:80, justifyContent:'center', alignItems:'center'}}>
@@ -135,26 +137,34 @@ export default class DrawerExample extends Component {
 
 							</View>
 
-						<View style={{ustifyContent:'center', alignItems:'center', margin:5, marginTop:20, backgroundColor:'#fff'}}>
-						<View style={{flexDirection:'row'}}>
-	                    <Text style={{flex:1, fontSize:12}}> BERITA ISLAMI </Text>
-	                    <Text style={{fontSize:10}}> Lihat Semua Artikel </Text>
-	                    </View>
-	                   <View style={{alignItems:'center', justifyContent:'center', width:350}}>
-	                    <Slideshow containerStyle={{marginTop:10}} position={1} arrowSize={1}
-						      dataSource={[
-						        { title: 'title 1',
-							    caption: 'caption 1',url:'http://placeimg.com/640/480/any' },
-													        { title: 'title 1',
-							    caption: 'caption 2',url:'http://placeimg.com/640/480/any' },
-													        { title: 'title 1',
-							    caption: 'caption 3',url:'http://placeimg.com/640/480/any' }
-						    ]} resizeMode="contain" height={130} />
-						 </View>
+							
+
+						
+
+	            </View>
+
+	                	
+					
+        </ImageBackground>
+        <View style={{ustifyContent:'center', alignItems:'center', margin:5, marginTop:20, backgroundColor:'#fff'}}>
+					<View style={{flexDirection:'row'}}>
+                    <Text style={{flex:1, fontSize:12}}> BERITA ISLAMI </Text>
+                    <Text style={{fontSize:10}}> Lihat Semua Artikel </Text>
+                    </View>
+					                   <View style={{alignItems:'center', justifyContent:'center', width:350}}>
+					                    <Slideshow containerStyle={{marginTop:10}} position={1} arrowSize={1}
+										      dataSource={[
+										        { title: 'title 1',
+											    caption: 'caption 1',url:'http://placeimg.com/640/480/animals' },
+																	        { title: 'title 1',
+											    caption: 'caption 2',url:'http://placeimg.com/640/480/animals' },
+																	        { title: 'title 1',
+											    caption: 'caption 3',url:'http://placeimg.com/640/480/animals' }
+										    ]} resizeMode="contain" height={130} />
+									  </View>
 						
 						</View>
-
-						<View style={{marginTop:5, backgroundColor:'#fff', alignItems:'flex-start'}}>
+         				<View style={{backgroundColor:'#fff', alignItems:'flex-start'}}>
 						<Text style={{textAlign:'left'}}>INFO KAJIAN</Text>
 						</View>
 
@@ -163,35 +173,16 @@ export default class DrawerExample extends Component {
 						</View>
 
 	                    
+					<Image source={require('../assets/banner_islampedia.png')} style={{borderRadius:5, height:60, width:350}} resizeMode="stretch" />
+	    </View>  
 
-
-	                    </View>
-
-
-
-
-	                	
-
-	                	
-						</View>
-                    </ImageBackground>
-				    
-
-
-							
-
-
-                        </View>
-
-			                    
                     </Content>
 
                     
-                    <View>
                         <View>
-                            
+                        <View>                          
                         </View>
-                    </View>
+                        </View>
                 
                 
             </Container>
