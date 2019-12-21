978906e218b5ed67c9d997f32816ec35a7fd633e
Fauzan <fauzan_lubis23@yahoo.com>
2019-12-21 21:31:19 +0800

    update quran

--- src/screens/quran/DetailSurah.js
+++ src/screens/quran/DetailSurah.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import dataSurah from '../../helper/en-id.pretty.json';
 
 
-function DetailSurah() {
+class DetailSurah extends Component{
+constructor(props){
+ super(props)
+
+}
+
+
+componentDidMount(){
+	
+console.log('ini datanya: ',dataSurah)
+		
+}
+
+render(){
   return(
       <View style={{ flex: 1 }}>
         <Text> Detail Surah </Text>
       </View>
       
   )
+}
 }
 
 export default DetailSurah;
--- src/screens/quran/ListSurah.js
+++ src/screens/quran/ListSurah.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, FlatList, ScrollView, TouchableOpacity} from 'react-native';
+import listSurahJson from '../../helper/surahs.pretty.json'
 
 
-function ListSurah() {
-  return(
+class ListSurah extends Component{
+
+constructor(props){
+	super(props)
+	
+}
+  
+
+
+  render(){
+  		return(
       <View style={{ flex: 1 }}>
-        <Text> List Surah </Text>
+        <FlatList 
+        data={listSurahJson}
+        extraData={this.state}
+        renderItem={({ item }) => (
+        	<TouchableOpacity onPress={()=>this.props.navigation.navigate('DetailSurah',{no: item.number})}>
+         	<Text> {item.transliteration_en + '('+item.translation_id+')'} </Text>
+         	</TouchableOpacity>
+        )}
+        />
       </View>
       
   )
+  }
+  
 }
 
 export default ListSurah;
