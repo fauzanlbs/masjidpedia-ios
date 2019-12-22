39bc7210c77ad0aca9767724078d93c896c3a648
Fauzan <fauzan_lubis23@yahoo.com>
2019-12-22 16:43:47 +0800

    update quran

--- src/screens/quran/DetailSurah.js
+++ src/screens/quran/DetailSurah.js
@@ -1,25 +1,60 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import dataSurah from '../../helper/en-id.pretty.json';
+
 
 
 class DetailSurah extends Component{
 constructor(props){
  super(props)
+ this.state = {
+ 	dataList : []
+ }
 
 }
 
+//  getData(){
+// 	let no =  this.props.navigation.state.params.no
+// 	let file =  `../../helper/surahs/${no}.json`
+// 	const surah =  file
+// 	return surah
+// }
+
+
 
 componentDidMount(){
-	
-console.log('ini datanya: ',dataSurah)
+
+
+const noSurah = this.props.navigation.state.params.no
+
+// let surahnya = await this.getData()
+// console.log('ini test', surahnya)
+// let data = require(surahnya)
+// let data =  import(surahnya)
+
+let dataFilter = dataSurah.filter(dt => dt.surah_number == noSurah);
+
+this.setState({dataList: dataFilter})
+console.log('filter', noSurah, dataFilter)
+
 		
 }
 
 render(){
   return(
       <View style={{ flex: 1 }}>
-        <Text> Detail Surah </Text>
+        <FlatList 
+        data={this.state.dataList}
+        extraData={this.state}
+        renderItem={({ item, index }) => 
+        	<View style={{padding:20,borderRadius:4, backgroundColor: (index % 2 == 0) ? '#ecf0f1' : '#fff'}}>
+         	<Text> {item.verse_number} {"\n"}</Text> 
+         	<Text style={{ fontFamily:'Amiri-Regular', fontSize:25}}> {item.text} {"\n"} </Text> 
+         	<Text style={{ fontFamily:'Amiri-Regular'}}> {item.translation_id} </Text>
+         	</View> 
+        }
+        keyExtractor={(item, index) => index.toString()}
+        />
       </View>
       
   )
--- src/screens/quran/ListSurah.js
+++ src/screens/quran/ListSurah.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList, ScrollView, TouchableOpacity} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity} from 'react-native';
 import listSurahJson from '../../helper/surahs.pretty.json'
 
 
@@ -18,11 +18,12 @@ constructor(props){
         <FlatList 
         data={listSurahJson}
         extraData={this.state}
-        renderItem={({ item }) => (
-        	<TouchableOpacity onPress={()=>this.props.navigation.navigate('DetailSurah',{no: item.number})}>
-         	<Text> {item.transliteration_en + '('+item.translation_id+')'} </Text>
+        renderItem={({ item, index }) => (
+        	<TouchableOpacity style={{backgroundColor:'lightgrey', borderWidth:0.1, elevation:1, padding:5, borderRadius:4, backgroundColor: (index % 2 == 0) ? '#ecf0f1' : '#fff'}} onPress={()=>this.props.navigation.navigate('DetailSurah',{no: item.number})}>
+         	<Text style={{ fontFamily:'Amiri-Regular'}}> {item.transliteration_en + '('+item.translation_id+')'} </Text>
          	</TouchableOpacity>
         )}
+        keyExtractor={(item, index) => index.toString()}
         />
       </View>
       
